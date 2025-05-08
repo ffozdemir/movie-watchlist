a96a93e14cab409721baed12c301730b7bcb7497
Fetullah Furkan Özdemir <furkanozdemir51@yandex.com.tr>
2025-05-08 22:36:06 +0200

    Refactor movie display layout and add styles for empty state

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+/* const fetchMovieData = async () =>{
+   const response = await fetch(`https://www.omdbapi.com/?apikey=${}}&s=blade%20runner`);
+   const data = await response.json();
+   console.log(data);
+
+} */
